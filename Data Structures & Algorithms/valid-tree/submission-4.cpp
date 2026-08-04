be38f697ce21d1e95f212ecba7e48b1e30a2a457
Ahmed Chennaoui <105203148+Ahmeed-Chennaoui@users.noreply.github.com>
2026-08-05 00:58:53 +0100

    Add: valid-tree - submission-4

---
+++ Data Structures & Algorithms/valid-tree/submission-4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+   public:
+    unordered_set<int> visit;
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if (edges.size() > n - 1) return false;
+        vector<vector<int>> adj(n);
+        for (const auto& v : edges) {
+            adj[v[0]].push_back(v[1]);
+            adj[v[1]].push_back(v[0]);
+        }
+        if (!dfs(0, -1, adj)) return false;
+
+        return visit.size()==n;
+    }
+
+    bool dfs(int node, int parent, vector<vector<int>>& adj) {
+        if (visit.count(node)) return false;
+        visit.insert(node);
+        for (auto x : adj[node]) {
+            if (x == parent) continue;
+            if (!dfs(x, node, adj)) return false;
+        }
+        return true;
+    }
+};
